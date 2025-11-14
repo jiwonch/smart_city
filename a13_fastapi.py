123c7b4b76a3b34697e22e3711ad020e44e2a087
jiwonch <rizjw26@gmail.com>
2025-11-14 09:23:59 +0900

    fastapi

--- a13_fastapi.py
+++ a13_fastapi.py
@@ -1,3 +1,25 @@
 # uv add fastapi uvicorn
 
-import fastapi
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, StreamingResponse
+
+app = FastAPI()
+
+
+# 간단한 HTML 페이지
+@app.get("/", response_class=HTMLResponse)
+def index():
+    return """
+    <html>
+        <head>
+            <title>OpenCV Stream</title>
+        </head>
+        <body>
+            <h1>OpenCV Video</h1>
+        </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
